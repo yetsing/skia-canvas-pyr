6247a7e28d629974fbe6401b7ba6b7f7112913c9
yetsing <heopro@163.com>
2026-04-13 00:27:03 +0800

    feat: Enhance CI workflow with multi-architecture container builds and add before-script for Linux

---
+++ containers/before_script_linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export TAG=$(make -s skia-version)
+git clone --depth 1 --branch $TAG --recurse-submodules -j8 https://github.com/rust-skia/rust-skia.git ../rust-skia
+patch -d ../rust-skia -p1 -F10 < ./containers/use-static-fontconfig.patch
+
+make -s with-local-skia
