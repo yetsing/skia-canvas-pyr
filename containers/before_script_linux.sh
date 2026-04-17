5b95b3521eecffc680e81641e58cb4fafd735d4e
yetsing <heopro@163.com>
2026-04-17 09:03:02 +0800

    fix: add COMMIT_ID argument to Dockerfiles and output to commit_id.txt

--- containers/before_script_linux.sh
+++ containers/before_script_linux.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+cat /opt/commit_id.txt
+
 export TAG=$(make -s skia-version)
 git clone --depth 1 --branch $TAG --recurse-submodules -j8 https://github.com/rust-skia/rust-skia.git ../rust-skia
 patch -d ../rust-skia -p1 -F10 < ./containers/use-static-fontconfig.patch
