73f13439acc7c9e777daf48df055f4596ad49cb7
yetsing <heopro@163.com>
2026-04-21 21:03:41 +0800

    fix: build error on musl

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-  // Only link libintl on musl targets (e.g., Alpine)
-  if std::env::var("CARGO_CFG_TARGET_ENV").as_deref() == Ok("musl") {
-    println!("cargo:rustc-link-lib=intl");
-  }
-}
